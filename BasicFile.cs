c87dd53913b49531bd5762084e326bfb48fab8e7
Seluor <78845320+Seluor@users.noreply.github.com>
2023-02-19 23:26:19 +0700

    Add files via upload

---
+++ BasicFile.cs
@@ -0,0 +1,27 @@
+﻿namespace BasicFile
+{
+    public class BasicDocument
+    {
+        public string Name { get; set; }
+        public string Authors { get; set; }
+        public string[] KeyWords { get; set; }
+        public string Theme { get; set; }
+        public string PathToFile { get; set; }
+
+        public virtual void PrintValues()
+        {
+            string str = "";
+
+            for (int index = 0; index < KeyWords.Length; ++index)
+            {
+                if (KeyWords[index] != "")
+                {
+                    str += KeyWords[index];
+                }
+            }
+
+            Console.WriteLine($"Name: {Name}\nAuthors: {Authors}\nKeyWords: {str}\nTheme: {Theme}\nPathToFile: {PathToFile}");
+            Console.ReadKey();
+        }
+    }
+}
